816ea204cf5a8a5248be9bbc3fd7d2738b996745
Jacopo Panerati <jacopo.panerati@utoronto.ca>
2026-05-22 12:20:31 -0400

    comment

--- tools_and_docs/sim_build.sh
+++ tools_and_docs/sim_build.sh
@@ -98,7 +98,7 @@ if [ "$BUILD_DOCKER" = "true" ]; then
   # Build common layers reused between images
   docker build $BUILD_ARGS --target ros2-image -t transitionary-ros2-image -f "${SCRIPT_DIR}/docker/aircraft.dockerfile" "${SCRIPT_DIR}/.."
   docker build $BUILD_ARGS --target ros2-qgc-image -t transitionary-ros2-qgc-image -f "${SCRIPT_DIR}/docker/ground.dockerfile" "${SCRIPT_DIR}/.."
-  # Three main images, the first build takes ~45'
+  # Build the 3 main images, the first build takes ~45'
   docker build $BUILD_ARGS -t aircraft-image -f "${SCRIPT_DIR}/docker/aircraft.dockerfile" "${SCRIPT_DIR}/.."
   docker build $BUILD_ARGS -t ground-image -f "${SCRIPT_DIR}/docker/ground.dockerfile" "${SCRIPT_DIR}/.."
   docker build $BUILD_ARGS -t simulation-image -f "${SCRIPT_DIR}/docker/simulation.dockerfile" "${SCRIPT_DIR}/.."
